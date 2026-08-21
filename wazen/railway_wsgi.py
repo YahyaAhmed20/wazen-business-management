b959cfcda981ad3aee81b52dc9adf4e802be7881
Yahya Ahmed <yahmdh6@gmail.com>
2026-08-21 18:56:13 +0300

    Fix Railway Frappe site resolution

--- wazen/railway_wsgi.py
+++ wazen/railway_wsgi.py
@@ -1,5 +1,9 @@
+import os
+
+import frappe
 import frappe.app
 
 frappe.app._sites_path = "/home/frappe/frappe-bench/sites"
+os.environ["FRAPPE_SITE_NAME_HEADER"] = "Host"
 
 application = frappe.app.application_with_statics()
